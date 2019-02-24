3777ddb75ad9be837d4e6b798c19a747c52f3586
Slavisa Babic <babic.slavisa@gmail.com>
2019-02-24 08:32:36 +0100

    Added new versions of code

--- OpenCVTutorial/main.cpp
+++ OpenCVTutorial/main.cpp
@@ -5,7 +5,7 @@
 using namespace cv;
 using namespace std;
 
- int main( int argc, char** argv )
+/* int main( int argc, char** argv )
  {
     VideoCapture cap(0); //capture the video from web cam
 
@@ -34,6 +34,10 @@ using namespace std;
   
   imshow("Original", imgOriginal); //show the original image
 
+
+	  
+
+
         if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
        {
             cout << "esc key is pressed by user" << endl;
@@ -43,4 +47,63 @@ using namespace std;
 
    return 0;
 
+}
+*/
+
+#include "opencv2/opencv.hpp"
+using namespace cv;
+int main(int, char**)
+{
+VideoCapture cap(0); // open the default camera
+if(!cap.isOpened())  // check if we succeeded
+return -1;
+Mat original;
+namedWindow("original",1);
+namedWindow("step1", 1);
+namedWindow("step2", 1);
+namedWindow("originalz", 1);
+for(;;)
+{
+Mat frame;
+cap >> frame; // get a new frame from camera
+imshow("originalz", frame);
+
+//cvtColor(frame, original, COLOR_BGR2GRAY);
+//imshow("original", original);
+//GaussianBlur(original, original, Size(7,7), 1.5, 1.5);
+//imshow("step1", original);
+//Canny(original, original, 0, 30, 3);
+//imshow("step2", original);
+if(waitKey(30) >= 0) break;
+}
+// the camera will be deinitialized automatically in VideoCapture destructor
+return 0;
+}
+Mat& ScanImageAndReduceC(Mat& I, const uchar* const table)
+{
+	// accept only char type matrices
+	CV_Assert(I.depth() == CV_8U);
+
+	int channels = I.channels();
+
+	int nRows = I.rows;
+	int nCols = I.cols * channels;
+
+	if (I.isContinuous())
+	{
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	int i, j;
+	uchar* p;
+	for (i = 0; i < nRows; ++i)
+	{
+		p = I.ptr<uchar>(i);
+		for (j = 0; j < nCols; ++j)
+		{
+			p[j] = table[p[j]];
+		}
+	}
+	return I;
 }
